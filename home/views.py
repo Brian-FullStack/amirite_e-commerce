57761a3466f9cd9adb16d6bc0468b2e6250ea416
Brian-FullStack <brian.fullstack@gmail.com>
2022-03-16 12:51:54 +0000

    Added header to base.html

--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# Render the home page
-def index(request) :
+
+def index(request):
+    '''
+    Render the home page
+    '''
     return render(request, 'home/index.html')
