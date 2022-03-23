b281baaf8c98f6b9d0fcc70725f3b95f0ebb7a0d
Brian-FullStack <brian.fullstack@gmail.com>
2022-03-23 12:21:01 +0000

    Added product details page

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 '''
 Necessary Imports
 '''
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -17,3 +17,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    '''
+    Display individual product details
+    '''
+    # return all products from the db
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
