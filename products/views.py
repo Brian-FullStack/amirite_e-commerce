fff083d47373e9bafcb6ab7c58447979ea7bd30c
Brian-FullStack <brian.fullstack@gmail.com>
2022-04-28 15:02:43 +0000

    Added category buttons for navigation

--- products/views.py
+++ products/views.py
@@ -14,7 +14,7 @@ def all_products(request):
     # return all products from the db
     products = Product.objects.all()
     query = None
-    categories = None
+    categories = Category.objects.all()
     sort = None
     direction = None
 
@@ -27,7 +27,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -48,13 +49,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'-{sort}_{direction}'
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current_sorting': current_sorting
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
