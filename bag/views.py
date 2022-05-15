4fa918aa1e636050921be38602f20b11d3266f2e
Brian-FullStack <brian.fullstack@gmail.com>
2022-05-15 15:38:57 +0000

    Added Update and remove items from the bag.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -34,5 +34,53 @@ def add_to_bag(request, item_id):
             bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def update_bag(request, item_id):
+    ''' Update the selected product in the shopping bag '''
+
+    quantity = int(request.POST.get('quantity'))
+    color = None
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
+    bag = request.session.get('bag', {})
+
+    if color:
+        if quantity > 0:
+            bag[item_id]['items_by_color'][color] = quantity
+        else:
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        color = None
+        if 'product_color' in request.POST:
+            color = request.POST['product_color']
+        bag = request.session.get('bag', {})
+
+        if color:
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
